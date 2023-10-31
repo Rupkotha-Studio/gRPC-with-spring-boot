4bb79d745fe7adb3104cb33158bfbabe11b0c2e5
DurjoyAcharya <da-durjoy@outlook.com>
2023-10-31 22:53:51 +0600

    grpc

---
+++ src/test/java/com/example/grpcwithspringboot/GRpcWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
